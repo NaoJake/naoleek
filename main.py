523ba4cc195f21a1b9b9ba802186eb1e01f54a18
antoine <karalarala@gmail.com>
2014-11-19 16:52:31 +0100

    matrice CDC

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print('coucou les amis')
-print('coucou les amis de naoLeek')
+from math import*
+
+#Définition de la matrice conformément à l'exemple du CDC
+matrice = [0,0,0,1,0,0,0,0],\
+          [0,0,0,1,1,1,0,0],\
+          [0,0,0,1,0,0,0,0],\
+          [0,0,0,1,1,0,0,0],\
+          [0,0,0,0,0,0,0,0],\
+          [0,0,0,0,1,0,0,0],\
+          [0,0,0,0,1,0,0,0],\
+          [0,0,0,0,1,0,0,0],
+
+
+
+print(matrice)
+
+ 
+#>>>
+#l'age de Eric Praline est incorrect
+#age de Henry Wensleydale = 50 , age de Eric Praline = 20
