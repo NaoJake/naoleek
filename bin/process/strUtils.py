e1009edf89d8bc8deeb6cfae130a4cbc36e4d807
jdwd <justine.dewilde.pro@gmail.com>
2015-06-12 17:26:02 +0200

    [JDWD]: algorithme du jeu + merge

--- bin/process/strUtils.py
+++ bin/process/strUtils.py
@@ -1,4 +1,4 @@
 __author__ = 'Justine Dewilde'
 
-def getPositionOfLetterInWord(letter, word)
+def getPositionOfLetterInWord(letter, word):
     "Retourne la liste des positions (0 à n) de la lettre dans le mot"
