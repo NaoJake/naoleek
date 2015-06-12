01398f3ef90121989c35000706009eff5e9324b6
jdwd <justine.dewilde.pro@gmail.com>
2015-06-12 17:40:36 +0200

    [JDWD]: fonction d'index des lettres pour écriture

--- bin/process/hangman/hangmanInit.py
+++ bin/process/hangman/hangmanInit.py
@@ -2,11 +2,10 @@ __author__ = 'Justine Dewilde'
 
 from naoqi import ALProxy
 from bin.process.hangman import hangmanGame
+from bin.process.strUtils import getPositionOfLetterInWord
 
 def playGame(motion):
     "fonction permettant de lancer le jeu"
-
-
 
     try:
         welcome = "Bonjour, nous allons jouer au pendu ! Je vais d'abord choisir un mot"
@@ -14,11 +13,13 @@ def playGame(motion):
         print(welcome)
         hmGame = hangmanGame.HangmanGame()
         randomWord = hmGame.getWordToFind()
-        #motion.say("Voila ! Ce mot a {0} lettres".format(str(len(randomWord))))
-        #motion.say("C'est parti !")
+        motion.say("Voila ! Ce mot a {0} lettres".format(str(len(randomWord))))
+        motion.say("C'est parti !")
         print("Voila ! Ce mot a {0} lettres".format(str(len(randomWord))))
         print("C'est parti !")
-        #ajouter la partie permettant d'ecrire les traits
+        #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        #ajouter la partie permettant d'ecrire les traits : nbr de traits > (len(randomWord)
+        #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         print(randomWord)
 
         #Partie
@@ -26,34 +27,27 @@ def playGame(motion):
             randomLetter = hmGame.getRandomLetter()
             hmGame.putLetter(randomLetter)
             if hmGame.correctLetter(randomLetter):
-               #motion.say("Bien ! La lettre {0} fait partie du mot".format(str(randomLetter)))
-               #fonction a mettre ici : nao ecrit la lettre
+                motion.say("Bien ! La lettre {0} fait partie du mot".format(str(randomLetter)))
+                #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                #fonction a mettre ici : nao ecrit la lettre : lettre > randomLetter & liste des positions :  getPositionOfLetterInWord(randomLetter, randomWord)
+                #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                 print("Bien ! La lettre {0} fait partie du mot".format(str(randomLetter)))
             else:
-                #motion.say("Perdu ! La lettre {0} ne fait pas partie du mot".format(str(randomLetter)))
+                motion.say("Perdu ! La lettre {0} ne fait pas partie du mot".format(str(randomLetter)))
                 print("Perdu ! La lettre {0} ne fait pas partie du mot".format(str(randomLetter)))
                 hmGame.nbrRemainingChances -= 1
 
-            #motion.say("Il te reste {0} chances".format(str(hmGame.nbrRemainingChances)))
+            motion.say("Il te reste {0} chances".format(str(hmGame.nbrRemainingChances)))
             print("Il te reste {0} chance(s)".format(str(hmGame.nbrRemainingChances)))
 
 
         #Fin de partie !
         if hmGame.didNaoLost():
-            #motion.say("Bravo ! Tu as reussi, le mot est {0} ".format(randomWord))
-             print("Bravo ! Tu as reussi, le mot est {0} ".format(randomWord))
+            motion.say("Bravo ! Tu as reussi, le mot est {0} ".format(randomWord))
+            print("Bravo ! Tu as reussi, le mot est {0} ".format(randomWord))
         if hmGame.didNaoWin() :
-            #motion.say("Oh ! Tu as perdu, le mot est {0} ".format(randomWord))
-             print("Oh ! Tu as perdu, le mot est {0} ".format(randomWord))
-
-        #sentence = "\RSPD="+ str( self.getParameter("Speed (%)") ) + "\ "
-        #sentence += "\VCT="+ str( self.getParameter("Voice shaping (%)") ) + "\ "
-        #sentence += str(p)
-        #sentence +=  "\RST\ "
-        # id = self.tts.post.say(str(sentence))
-        #self.ids.append(id)
-        #self.tts.wait(id, 0)
-
+            motion.say("Oh ! Tu as perdu, le mot est {0} ".format(randomWord))
+            print("Oh ! Tu as perdu, le mot est {0} ".format(randomWord))
 
     except BaseException as err:
         print(err)
--- bin/process/strUtils.py
+++ bin/process/strUtils.py
@@ -2,3 +2,12 @@ __author__ = 'Justine Dewilde'
 
 def getPositionOfLetterInWord(letter, word):
     "Retourne la liste des positions (0 à n) de la lettre dans le mot"
+    i = 0
+    result = []
+    while i < len(word):
+        if(word[i] == letter):
+            result.append(i)
+        else:
+            i += 1
+
+    return result
